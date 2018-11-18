cb6a8482681e2290b4078e197fbfcff63097ff53
Vladyslav Samodelok <vladfux4@gmail.com>
2018-11-18 18:15:54 +0200

    cmake files

---
+++ src/kernel/scheduler/routine.cc
@@ -0,0 +1,27 @@
+/*=============================================================================
+Project Z - Operating system for ARM processors
+Copyright (C) 2018 Vladyslav Samodelok <vladfux4@gmail.com>
+All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+=============================================================================*/
+#include "scheduler/routine.h"
+
+namespace scheduler {
+
+Routine::Routine() {
+}
+
+Routine::~Routine() {
+}
+
+}  // namespace scheduler
---
+++ src/kernel/scheduler/routine.h
@@ -0,0 +1,45 @@
+/*=============================================================================
+Project Z - Operating system for ARM processors
+Copyright (C) 2018 Vladyslav Samodelok <vladfux4@gmail.com>
+All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+=============================================================================*/
+#ifndef KERNEL_SCHEDULER_ROUTINE_H_
+#define KERNEL_SCHEDULER_ROUTINE_H_
+
+namespace scheduler {
+
+/**
+ * @brief The Routine class
+ */
+class Routine {
+ public:
+  /**
+   * @brief Routine
+   */
+  Routine();
+
+  /**
+   * @brief Routine
+   */
+  virtual ~Routine();
+
+  /**
+   * @brief Exec
+   */
+  virtual void Exec() = 0;
+};
+
+}  // namespace scheduler
+
+#endif  // KERNEL_SCHEDULER_ROUTINE_H_
